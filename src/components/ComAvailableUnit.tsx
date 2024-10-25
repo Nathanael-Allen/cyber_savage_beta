ce84dc4dc141e9bc269cc6265e170625ad15441d
Nathanael Allen <nathanael.allen1@gmail.com>
2024-10-25 09:04:10 -0700

    added number of spells to unit selection list

--- src/components/ComAvailableUnit.tsx
+++ src/components/ComAvailableUnit.tsx
@@ -16,12 +16,13 @@ export default function ComAvailableUnit({ unit, handleAddUnit }: UnitProps) {
   return (
     <div
       onClick={() => handleAddUnit(unit)}
-      className="grid grid-cols-4 rounded-md p-2 mt-1 mb-1 border border-black hover:bg-gray-200 cursor-pointer">
+      className="grid grid-cols-4 rounded-md p-2 mt-1 mb-1 border border-black hover:bg-gray-200 cursor-pointer"
+    >
       <h4 className="font-semibold col-span-3 text-lg">{unit.type}</h4>
-      <div className="flex gap-2  items-center col-span-3 row-start-2"
-       >
+      <div className="flex gap-2  items-center col-span-3 row-start-2">
         {equipList}
         <p>{unit.numTraits} trait(s)</p>
+        {unit.numSpells && <p className="text-sky-700">{unit.numSpells} spell(s)</p>}
       </div>
       <button
         className="text-sky-600 text-xl font-semibold col-start-4 row-start-1 row-span-2"
