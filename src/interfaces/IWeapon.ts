c50c75519b152dcf9a8ce392b898466f83477067
Nathanael Allen <nathanael.allen1@gmail.com>
2024-10-26 17:23:48 -0700

    added weapon component

--- src/interfaces/IWeapon.ts
+++ src/interfaces/IWeapon.ts
@@ -2,7 +2,7 @@ interface IWeapon {
   type: "ranged" | "melee" | "unarmed";
   techLevel: "simple" | "standard" | "advanced";
   numTraits?: number;
-  subType?: "light" | "medium" | "heavy"
+  subtype?: "light" | "medium" | "heavy"
 };
 
 export default IWeapon;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import ComWeapon from "./components/ComWeapon.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ComWeapon weapon={{type: "ranged", techLevel: "advanced"}} />
   </StrictMode>
 );
+
+{/* <App /> */}
